e95d67216d196b82ab0bfa1724af45b0dde4b9af
Henrik Tudborg <henrik@tudb.org>
2013-09-04 02:07:57 +0200

    adding autostamping to setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 
 from setuptools import setup, find_packages
 import asynckit
+import time
+
+def stamp():
+    return time.strftime('%j%H%M')
 
 setup(
     name         = 'asynckit',
-    version      = asynckit.__version__,
+    version      = asynckit.__version__+'.{}'.format(stamp()), #autostamp to avoid overwriting old versions accidentally
     description  =  '''AsyncKit is a micro-toolkit for doing async work in python 
                    (in your otherwise hacked togethe synchronous single-file script)''',
     author       = asynckit.__author__,
