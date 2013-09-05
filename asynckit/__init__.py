e95481fb9c003c7452d6eb8141343cd95612545b
Henrik Tudborg <henrik@tudb.org>
2013-09-05 17:49:12 +0200

    bumping to 0.3.1

--- asynckit/__init__.py
+++ asynckit/__init__.py
@@ -4,6 +4,6 @@ from value  import AsyncValue
 
 __author__     = "Henrik Tudborg"
 __license__    = "MIT"
-__version__    = "0.3.0"
+__version__    = "0.3.1"
 __maintainer__ = "Henrik Tudborg"
 __email__      = "henrik@tudb.org"
