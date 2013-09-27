6474d26a6a8dc54977cb1cf26dc74762005b90c7
Henrik Tudborg <henrik@tudb.org>
2013-09-27 18:11:34 +0200

    the chaining is worthy of a minor version bump

--- asynckit/__init__.py
+++ asynckit/__init__.py
@@ -4,6 +4,6 @@ from value  import AsyncValue
 
 __author__     = "Henrik Tudborg"
 __license__    = "MIT"
-__version__    = "0.3.3"
+__version__    = "0.4.0"
 __maintainer__ = "Henrik Tudborg"
 __email__      = "henrik@tudb.org"
